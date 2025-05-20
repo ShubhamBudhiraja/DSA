a64522b19c2e770201f1707153c1a5b72aa5b7f9
Shubham Budhiraja <shubhambudhiraja85@gmail.com>
2025-05-20 20:27:03 +0530

    tree - check identical trees

---
+++ questions/tree/identical.js
@@ -0,0 +1,25 @@
+import { Node } from "../../concepts/tree/common.js";
+
+const checkIdentical = (root1, root2) => {
+    if (!root1 && !root2) return true;
+    if (!root1 || !root2) return false;
+    if (root1.value !== root2.value) return false;
+
+    return (
+        checkIdentical(root1.left, root2.left) &&
+        checkIdentical(root1.right, root2.right)
+    );
+};
+
+const root = new Node(2);
+root.right = new Node(3);
+root.left = new Node(1);
+root.left.right = new Node(5);
+root.left.left = new Node(4);
+
+const root2 = new Node(2);
+root2.right = new Node(3);
+root2.left = new Node(1);
+root2.left.right = new Node(5);
+
+console.log(checkIdentical(root, root2));
